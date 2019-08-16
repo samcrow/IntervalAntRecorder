376c11a9325647c5d2b3a59c3ab6004d4e912911
Sam Crow <scrow@eng.ucsd.edu>
2019-08-16 16:38:57 -0700

    Things

--- app/src/main/java/org/samcrow/antrecorder/MainActivity.java
+++ app/src/main/java/org/samcrow/antrecorder/MainActivity.java
@@ -1,205 +1,156 @@
 package org.samcrow.antrecorder;
 
-import android.content.DialogInterface;
-import android.graphics.Color;
-import android.graphics.PorterDuff.Mode;
 import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.view.ViewCompat;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.MenuItem.OnMenuItemClickListener;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
-import org.joda.time.DateTime;
-import org.samcrow.antrecorder.Event.Type;
-
-import java.io.File;
-import java.io.IOException;
 import java.util.Locale;
-import java.util.Queue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
- * An activity for recording events
- *
- * This activity must be started with an extra with key {@link #EXTRA_FILE_PATH} corresponding to
- * the path to the file to write to.
- *
- * It may be given an extra with key {@link #EXTRA_DATA_SET} containing the data set name.
+ * An activity for counting events
  */
 public class MainActivity extends AppCompatActivity {
 	private static final String TAG = MainActivity.class.getSimpleName();
+
 	/**
 	 * The volume level for sounds
 	 */
 	private static final float VOLUME = 0.5f;
 
-	public static final String EXTRA_FILE_PATH = MainActivity.class.getName() + ".EXTRA_FILE_PATH";
-	public static final String EXTRA_DATA_SET = MainActivity.class.getName() + ".EXTRA_DATA_SET";
-
 	private TextView mInCountField;
 	private TextView mOutCountField;
+
+	/** Field to display countdown */
+	private TextView mTimeField;
+
+
+    private Button mInButton;
+    private Button mOutButton;
+    private Button mStartButton;
 
 	private SoundPool mSoundPool;
 	private int mInSoundId;
 	private int mOutSoundId;
+	private int mCountdownExpiredSoundId;
 
-	/**
-	 * The service that runs file operations
-	 */
-	@SuppressWarnings("FieldCanBeLocal")
-	private ExecutorService mExecutor;
+	private int mInCount = 0;
+	private int mOutCount = 0;
+    /**
+     * Seconds remaining in the interval, rounded down
+     */
+	private int mSeconds = 0;
 
-	/**
-	 * A thread-safe queue storing the events to be written
-	 */
-	private Queue<FileAction> mFileQueue;
+    /**
+     * The timer used to count down
+     */
+    private Timer mTimer;
 
-	/**
-	 * A click listener that calls {@link #finish()} on this activity
-	 */
-	private final DialogInterface.OnClickListener FINISH_LISTENER = new DialogInterface.OnClickListener() {
-		@Override
-		public void onClick(DialogInterface dialog, int which) {
-			finish();
-		}
-	};
-	private EventWriter mWriter;
 
-	@Override
+    @Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		final ActionBar bar = getSupportActionBar();
-		if (bar != null) {
-			bar.setDisplayHomeAsUpEnabled(true);
-		}
-
-		final String dataSetName = getIntent().getStringExtra(EXTRA_DATA_SET);
-		if (dataSetName != null) {
-			setTitle(dataSetName);
-		}
-
-		final String filePath = getIntent().getStringExtra(EXTRA_FILE_PATH);
-		if (filePath == null) {
-			throw new IllegalStateException("This activity must be started with a file path extra");
-		}
-		// Set up the file writer
-		try {
-			mExecutor = Executors.newSingleThreadExecutor();
-			mWriter = new EventWriter(new File(filePath), new EventWriter.WriteHandler() {
-				@Override
-				public void handleException(IOException e) {
-					new AlertDialog.Builder(MainActivity.this)
-							.setTitle("Failed to write event")
-							.setMessage(e.getLocalizedMessage())
-							.setNeutralButton(android.R.string.ok, FINISH_LISTENER)
-							.show();
-				}
-
-				@Override
-				public void countsUpdated(int inCount, int outCount) {
-					mInCountField.setText(String.format(Locale.getDefault(), "%d", inCount));
-					mOutCountField.setText(String.format(Locale.getDefault(), "%d", outCount));
-				}
-			});
-			mFileQueue = mWriter.getQueue();
-			mExecutor.submit(mWriter);
-		} catch (IOException e) {
-			new AlertDialog.Builder(this)
-					.setTitle("Failed to open file")
-					.setMessage(e.getLocalizedMessage())
-					.setNeutralButton(android.R.string.ok, FINISH_LISTENER);
-		}
-
-
-		Button inButton = (Button) findViewById(R.id.in_button);
-		Button outButton = (Button) findViewById(R.id.out_button);
+        mInButton = (Button) findViewById(R.id.in_button);
+        mOutButton = (Button) findViewById(R.id.out_button);
+        mStartButton = (Button) findViewById(R.id.start_button);
 		mInCountField = (TextView) findViewById(R.id.in_count);
 		mOutCountField = (TextView) findViewById(R.id.out_count);
+		mTimeField = (TextView) findViewById(R.id.time_label);
 
-		ViewCompat.setBackgroundTintList(inButton, getResources().getColorStateList(R.color.in_background));
-		ViewCompat.setBackgroundTintList(outButton, getResources().getColorStateList(R.color.out_background));
+		ViewCompat.setBackgroundTintList(mInButton, getResources().getColorStateList(R.color.in_background));
+		ViewCompat.setBackgroundTintList(mOutButton, getResources().getColorStateList(R.color.out_background));
 
+		setInCount(0);
+		setOutCount(0);
 
-		inButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				saveEvent(new Event(Type.AntIn, DateTime.now()));
-				mSoundPool.play(mInSoundId, VOLUME, VOLUME, 1, 0, 1);
-			}
-		});
-		outButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				saveEvent(new Event(Type.AntOut, DateTime.now()));
-				mSoundPool.play(mOutSoundId, VOLUME, VOLUME, 1, 0, 1);
-			}
-		});
+		mInButton.setEnabled(false);
+		mOutButton.setEnabled(false);
 
-		// Set up mSoundPool
+		// Set up sound pool
 		mSoundPool = new SoundPool(4, AudioManager.STREAM_SYSTEM, 0);
 		mInSoundId = mSoundPool.load(this, R.raw.ping_high, 1);
 		mOutSoundId = mSoundPool.load(this, R.raw.ping_low, 1);
+		mCountdownExpiredSoundId = mSoundPool.load(this, R.raw.sound_notification_double, 1);
 	}
 
-	@Override
-	protected void onPause() {
-		super.onPause();
-	}
+	public void onInButtonClicked(View v) {
+        setInCount(mInCount + 1);
+        mSoundPool.play(mInSoundId, VOLUME, VOLUME, 1, 0, 1);
+    }
 
-	@Override
-	protected void onResume() {
-		super.onResume();
-	}
+    public void onOutButtonClicked(View v) {
+        setOutCount(mOutCount + 1);
+        mSoundPool.play(mOutSoundId, VOLUME, VOLUME, 1, 0, 1);
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_main, menu);
+    public void onStartButtonClicked(View v) {
+        setInCount(0);
+        setOutCount(0);
+        setSeconds(29);
+        mStartButton.setEnabled(false);
+        mInButton.setEnabled(true);
+        mOutButton.setEnabled(true);
+        mTimer = new Timer();
+        mTimer.scheduleAtFixedRate(new CountdownTask(), 1000, 1000);
+    }
 
-		/*
-	  The menu item used to delete entries
-	 */
-		MenuItem deleteItem = menu.findItem(R.id.action_delete);
-		deleteItem.setOnMenuItemClickListener(new OnMenuItemClickListener() {
-			@Override
-			public boolean onMenuItemClick(MenuItem item) {
-				mFileQueue.add(FileAction.deleteLast());
-				return true;
-			}
-		});
+    private void setSeconds(int seconds) {
+        mSeconds = seconds;
+        mTimeField.post(new Runnable() {
+            @Override
+            public void run() {
+                mTimeField.setText(String.format(Locale.getDefault(), "%d", mSeconds));
+            }
+        });
+    }
 
-		return true;
-	}
+    private void setInCount(int count) {
+        mInCount = count;
+        mInCountField.setText(String.format(Locale.getDefault(), "%d", mInCount));
+    }
+    private void setOutCount(int count) {
+        mOutCount = count;
+        mOutCountField.setText(String.format(Locale.getDefault(), "%d", mOutCount));
+    }
 
-	private void saveEvent(@NonNull Event event) {
-		if (!mWriter.isRunning()) {
-			new AlertDialog.Builder(this)
-					.setTitle("Writer not running")
-					.setMessage("Due to an internal error, the writer task is not running.")
-					.setNeutralButton(android.R.string.ok, FINISH_LISTENER)
-					.show();
-		}
-		if (mFileQueue.size() > 8) {
-			new AlertDialog.Builder(this)
-					.setTitle("Writer not keeping up")
-					.setMessage("The writer task has not kept up with events.")
-					.setNeutralButton(android.R.string.ok, FINISH_LISTENER)
-					.show();
-		}
-		mFileQueue.add(FileAction.event(event));
-	}
+    private class CountdownTask extends TimerTask {
+
+        @Override
+        public void run() {
+            if (mSeconds == 0) {
+                // Stop counting
+                mTimer.cancel();
+                mTimer = null;
+                mStartButton.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mStartButton.setEnabled(true);
+                        mSoundPool.play(mCountdownExpiredSoundId, VOLUME, VOLUME, 1, 0, 1);
+                    }
+                });
+                mInButton.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mInButton.setEnabled(false);
+                    }
+                });
+                mOutButton.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mOutButton.setEnabled(false);
+                    }
+                });
+            } else {
+                setSeconds(mSeconds - 1);
+            }
+        }
+    }
 }
